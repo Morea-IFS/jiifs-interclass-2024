38a6b3b6ed473ec26bd4a0388ac15f5d47502cfd
Alexandre-Michael <alexandremi2024@gmail.com>
2024-10-02 16:30:38 -0300

    🔨 Search script done

--- static/js/script.js
+++ static/js/script.js
@@ -34,3 +34,25 @@ function toggle(source) {
         checkboxes[i].checked = source.checked;
     }
 };
+
+function search_table() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search-input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("matches-table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {  // Começa em 1 para pular o cabeçalho
+        tr[i].style.display = "none";  // Oculta a linha por padrão
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+            if (td[j]) {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";  // Mostra a linha se houver correspondência
+                    break;
+                }
+            }
+        }
+    }
+}
