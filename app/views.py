b259e7701a77963529c9bd1979bc0eeab42f82d3
Alexandre-Michael <alexandremi2024@gmail.com>
2024-10-09 12:41:05 -0300

    🚚 Renamed urls and new route.

--- app/views.py
+++ app/views.py
@@ -5,45 +5,49 @@ def index(request):
     return render(request, 'index.html')
 
 def player_manage(request):
-    return render(request, 'gerenciamento-jogadores.html')
+    return render(request, 'player_manage.html')
 
 def team_manage(request):
-    return render(request, 'gerenciamento-times.html')
+    return render(request, 'team_manage.html')
 
 def team_players_manage(request):
-    return render(request, 'gerenciamento-jogadores-times.html')
+    return render(request, 'team_players_manage.html')
 
 def matches_manage(request):
-    return render(request, 'gerenciamento-partidas.html')
+    return render(request, 'matches_manage.html')
 
 def matches_edit(request):
-    return render(request, 'editar-partidas.html')
+    return render(request, 'matches_edit.html')
 
 def matches_register(request):
-    return render(request, 'cadastro-partidas.html')
+    return render(request, 'matches_register.html')
 
 def add_player_team(request):
-    return render(request, 'adicionar-jogadores-time.html')
+    return render(request, 'add_player_team.html')
+
 def player_register(request):
-    return render(request, 'cadastro-jogadores.html')
+    return render(request, 'player_register.html')
 
 def team_register(request):
-    return render(request, 'cadastro-times.html')
+    return render(request, 'team_register.html')
 
 def player_edit(request):
-    return render(request, 'editar-jogadores.html')
+    return render(request, 'player_edit.html')
 
 def team_edit(request):
-    return render(request, 'editar-times.html')
+    return render(request, 'team_edit.html')
 
-def game(request):
-    return render(request, 'jogo.html')
+def games(request):
+    return render(request, 'games.html')
 
 def sport_manage(request):
-    return render(request, 'gerenciamento-esportes.html')
+    return render(request, 'sport_manage.html')
 
 def sport_edit(request):
-    return render(request, 'editar-esportes.html')
+    return render(request, 'sport_edit.html')
 
 def sport_register(request):
-    return render(request, 'cadastro-esportes.html')
+    return render(request, 'sport_register.html')
+
+def general_data(request):
+    return render(request, 'general_data.html')
