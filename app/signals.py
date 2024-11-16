1236169aaac9b910676d797cce92d0e1432537f0
Silas-Prog <152338397+Silas-Prog@users.noreply.github.com>
2024-11-15 22:58:04 -0300

    :construction: More features aligned with websocket

--- app/signals.py
+++ app/signals.py
@@ -1,51 +1,21 @@
 # app/signals.py
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.conf import settings
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from .models import Point, Match, Team_match
 
-@receiver(post_save, sender=Point)
-def point_saved(sender, instance, created, **kwargs):
-    # Recupere a partida atual
-    if created:
-        match = Match.objects.get(status=1)
-        team_matchs = Team_match.objects.filter(match=match)
-        team_match_a = team_matchs[0]
-        team_match_b = team_matchs[1]
-        
-        # Conte os pontos
-        point_a = Point.objects.filter(team_match=team_match_a).count()
-        point_b = Point.objects.filter(team_match=team_match_b).count()
+default_photo_url = f"{settings.MEDIA_URL}defaults/person.png"
 
-        # Envia dados atualizados para o WebSocket
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            "placar",
-            {
-                'type': 'send_score',
-                'score': {
-                    'team_a': team_match_a.team.name,
-                    'team_b': team_match_b.team.name,
-                    'points_a': point_a,
-                    'points_b': point_b,
-                },
-            }
-        )
-
-@receiver(post_delete, sender=Point)
+@receiver([post_save,post_delete], sender=Point)
 def point_removed(sender, instance, using, **kwargs):
-    # Recupere a partida atual
     match = Match.objects.get(status=1)
     team_matchs = Team_match.objects.filter(match=match)
     team_match_a = team_matchs[0]
     team_match_b = team_matchs[1]
-    
-    # Conte os pontos
     point_a = Point.objects.filter(team_match=team_match_a).count()
     point_b = Point.objects.filter(team_match=team_match_b).count()
-
-    # Envia dados atualizados para o WebSocket
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         "placar",
@@ -56,6 +26,35 @@ def point_removed(sender, instance, using, **kwargs):
                 'team_b': team_match_b.team.name,
                 'points_a': point_a,
                 'points_b': point_b,
+                'photoA': team_match_a.team.photo.url,
+                'photoB': team_match_b.team.photo.url,
             },
         }
     )
+
+@receiver(post_save, sender=Match)
+def match_status_updated(sender, instance, created, **kwargs):
+    if Match.objects.filter(status=1):
+        match = Match.objects.get(status=1)
+        team_matchs = Team_match.objects.filter(match=match)
+        team_match_a = team_matchs[0]
+        team_match_b = team_matchs[1] 
+        point_a = Point.objects.filter(team_match=team_match_a).count()
+        point_b = Point.objects.filter(team_match=team_match_b).count()
+        channel_layer = get_channel_layer()
+        match_data = {
+            'team_a': team_match_a.team.name,
+            'team_b': team_match_b.team.name,
+            'points_a': point_a,
+            'points_b': point_b,
+        }
+        match_data['photoA'] = team_match_a.team.photo.url if team_match_a.team.photo else default_photo_url
+        match_data['photoB'] = team_match_b.team.photo.url if team_match_b.team.photo else default_photo_url
+
+        async_to_sync(channel_layer.group_send)(
+            'placar',  # Nome do grupo para enviar a mensagem
+            {
+                'type': 'match_update',
+                'match': match_data,
+            }
+        )
