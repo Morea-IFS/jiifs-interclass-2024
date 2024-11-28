9bb84fd31dda66695a044b7323bc10f6a55fb726
Vineees <marcosvinicius.sm@icloud.com>
2024-11-28 01:15:25 -0300

    🐛 Fixed problem loading debug
    It was set to debug false

--- interclasse/settings.py
+++ interclasse/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+
 
 ALLOWED_HOSTS = [os.getenv('HOST')]
 
